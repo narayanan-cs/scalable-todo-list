b9ced1c64c147f12854d64ad924f3048b6732bc4
Narayanan <narayanan.cs.38@gmail.com>
2025-07-10 22:19:35 +0530

    remove dummy clientId and clientSecret to test file

--- ScalableToDoList/tests/apiGateway.test.spec.ts
+++ ScalableToDoList/tests/apiGateway.test.spec.ts
@@ -2,8 +2,7 @@ const request = require('supertest');
 const nock = require('nock');
 const app = require('../src/gateway.ts'); // Adjust the path to your app file
 const mongoose = require('mongoose');
-process.env.CLIENT_ID = 'dummy';
-process.env.CLIENT_SECRET = 'dummy';
+
 describe('Todo API', () => {
     let cookie: string; // Declare the cookie variable here
     let todoId: string;
